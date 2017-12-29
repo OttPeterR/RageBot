148b9f199754342fe2ae1856e7d16c6e0a50a4ba
Peter Ott <ott.peter.r@gmail.com>
2017-12-28 20:54:59 -0500

    reads bot token from text file

--- server.py
+++ server.py
@@ -5,19 +5,40 @@ from flask import Flask, request
 app = Flask(__name__)
 
 if len(sys.argv) != 2:
-    print("usage:\n\tpython server.py <your-bot-id>")
+    print("usage:\n\texport FLASK_APP=server.py\n\tflask run")
     exit()
 
-bot_token = sys.argv[1]
+
+token_file = open("bot_token.txt", 'r')
+bot_token =  str(token_file.read())
+token_file.close()
+
+
 
 def get_url(method):
-  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+    return "https://api.telegram.org/bot{}/{}".format(bot_token, method)
+
 
 def process_message(update):
+    # update dictionary:
+    # {u'message':
+    #   {u'date': 1234567890, u'text': u'Message-Here',
+    #   u'from': {u'username': u'User-Here', u'first_name': u'Name-Here',
+    #   u'last_name': u'LastName-Here', u'is_bot': False,
+    #   u'language_code': u'en', u'id': 1234567890}, u'message_id': 1234567890,
+    #   u'chat':
+    #     {u'username': u'User-Here', u'first_name': u'Name-Here',
+    #     u'last_name': u'LaseName-Here', u'type': u'private',
+    #     u'id': 1234567890}
+    #   },
+    #   u'update_id': 1234567890}
     data = {}
+    print update
     data["chat_id"] = update["message"]["from"]["id"]
     data["text"] = "I can hear you!"
     r = requests.post(get_url("sendMessage"), data=data)
+    print r
+
 
 @app.route("/{}".format(bot_token), methods=["POST"])
 def process_update():
@@ -26,4 +47,3 @@ def process_update():
         if "message" in update:
             process_message(update)
         return "ok!", 200
-
