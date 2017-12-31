806779adafa4ce04ba003ea6b0847b33a311e876
Peter Ott <ott.peter.r@gmail.com>
2017-12-31 00:48:58 -0500

    nothing much

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import sys
 import requests
 from flask import Flask, request
+from sentiment_analysis import analyzer
 
 app = Flask(__name__)
+
 
 if len(sys.argv) != 2:
     print("usage:\n\texport FLASK_APP=server.py\n\tflask run")
@@ -10,9 +12,18 @@ if len(sys.argv) != 2:
 
 
 token_file = open("bot_token.txt", 'r')
-bot_token =  str(token_file.read())
+bot_token = str(token_file.read())
 token_file.close()
 
+
+def __init():
+    global user_rage
+    global analyzer
+    user_rage = {}
+    analyzer = analyzer()
+
+
+__init()
 
 
 def get_url(method):
@@ -23,21 +34,25 @@ def process_message(update):
     # update dictionary:
     # {u'message':
     #   {u'date': 1234567890, u'text': u'Message-Here',
-    #   u'from': {u'username': u'User-Here', u'first_name': u'Name-Here',
-    #   u'last_name': u'LastName-Here', u'is_bot': False,
-    #   u'language_code': u'en', u'id': 1234567890}, u'message_id': 1234567890,
-    #   u'chat':
+    #   u'from':
     #     {u'username': u'User-Here', u'first_name': u'Name-Here',
-    #     u'last_name': u'LaseName-Here', u'type': u'private',
-    #     u'id': 1234567890}
+    #     u'last_name': u'LastName-Here', u'is_bot': False,
+    #     u'language_code': u'en', u'id': 1234567890},
+    #     u'message_id': 1234567890,
+    #     u'chat':
+    #       {u'username': u'User-He re', u'first_name': u'Name-Here',
+    #       u'last_name': u'LaseName-Here', u'type': u'private',
+    #       u'id': 1234567890}
     #   },
     #   u'update_id': 1234567890}
     data = {}
     print update
     data["chat_id"] = update["message"]["from"]["id"]
-    data["text"] = "I can hear you!"
-    r = requests.post(get_url("sendMessage"), data=data)
-    print r
+    message_text = update['message']['text']
+    score = analyzer.score_sentiment(message_text)
+    if message_text == "yo":
+        data["text"] = "yo yo man dawg"
+        requests.post(get_url("sendMessage"), data=data)
 
 
 @app.route("/{}".format(bot_token), methods=["POST"])
