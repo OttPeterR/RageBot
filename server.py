1287c2074ce8b7bf8ed60d7e1af391d54f5d85d0
Peter Ott <ott.peter.r@gmail.com>
2017-12-31 01:36:14 -0500

    passes message to RageBot to handle it

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 from flask import Flask, request
-from sentiment_analysis import analyzer
+from RageBot import RageBot
 
 app = Flask(__name__)
 
@@ -14,16 +14,7 @@ if len(sys.argv) != 2:
 token_file = open("bot_token.txt", 'r')
 bot_token = str(token_file.read())
 token_file.close()
-
-
-def __init():
-    global user_rage
-    global analyzer
-    user_rage = {}
-    analyzer = analyzer()
-
-
-__init()
+ragebot = RageBot()
 
 
 def get_url(method):
@@ -31,27 +22,11 @@ def get_url(method):
 
 
 def process_message(update):
-    # update dictionary:
-    # {u'message':
-    #   {u'date': 1234567890, u'text': u'Message-Here',
-    #   u'from':
-    #     {u'username': u'User-Here', u'first_name': u'Name-Here',
-    #     u'last_name': u'LastName-Here', u'is_bot': False,
-    #     u'language_code': u'en', u'id': 1234567890},
-    #     u'message_id': 1234567890,
-    #     u'chat':
-    #       {u'username': u'User-He re', u'first_name': u'Name-Here',
-    #       u'last_name': u'LaseName-Here', u'type': u'private',
-    #       u'id': 1234567890}
-    #   },
-    #   u'update_id': 1234567890}
     data = {}
-    print update
     data["chat_id"] = update["message"]["from"]["id"]
-    message_text = update['message']['text']
-    score = analyzer.score_sentiment(message_text)
-    if message_text == "yo":
-        data["text"] = "yo yo man dawg"
+    response = ragebot.pass_message(update)
+    if response is not None:
+        data["text"] = response
         requests.post(get_url("sendMessage"), data=data)
 
 
