ae262664ec7056ebbc96942d9c751d132ca01cf2
Peter Ott <ott.peter.r@gmail.com>
2017-12-31 01:36:42 -0500

    takes in message, passes it to sentiment_analysis if needed, returns values if its apropriate

--- RageBot.py
+++ RageBot.py
@@ -1,17 +1,23 @@
-import telepot # for telegram use
-
-def __handle_message(msg):
-    print msg
+from sentiment_analysis import analyzer
 
 
+class RageBot:
 
+    def __init__(self):
+        self.user_rage_scores = {}
+        self.analyzer = analyzer()
 
-
-if __name__ == '__main__':
-    global bot
-
-    startUp()
-    bot = telepot.Bot(ConfigHelper.getBotID())
-    bot.message_loop(__handle_message())
-    print('%s is online...' % name)
-    
+    def pass_message(self, update):
+        message_text = update['message']['text']
+        if(message_text[0] == '/'):
+            # the message is a command
+            if(message_text[:11] == "/scoreThis "):
+                score = self.analyzer.score_sentiment(message_text[11:])
+                return str(score.polarity)
+            elif(message_text == "/myRage"):
+                # lookup the chat and user and return their current rage rank
+                return "--- under construction ---"
+        else:
+            score = self.analyzer.score_sentiment(message_text)
+            # put it into the dictionary of users and their scores
+            return None
